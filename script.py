b3fd08d88c92194a8f94d5f3f1315ed96c90600b
Mihir Shah <cs-mihir@users.noreply.github.com>
2016-07-19 15:21:49 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,13 @@
 import urllib2
-#import pdb;pdb.set_trace()
 for i in range(1,50):
 	url = 'https://ecampus.daiict.ac.in/webapp/intranet/StudentPhotos/'
 	ext = '2015010'+str(i)+'.jpg'
-	if i<10:
+	if i < 10:
 		ext = '20150100'+str(i)+'.jpg'
 	url += ext
-	print url
+	#Here you can specify your url also from where you want to download jpg file
 	file = open(str(i)+'.jpg','wb')
 	file.write(urllib2.urlopen(url).read())
 	file.close()
 	print 'Image of '+str(i)+' downloaded'
-
 print 'Success'
